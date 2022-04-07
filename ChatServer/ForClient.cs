cd4eec57b41338e153b42f4a5668525f62e56dca
BakaShanNei <shannei@elyart.cn>
2022-04-08 03:40:19 +0800

    4月8日 交工程

---
+++ ChatServer/ForClient.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class ForClient
+{
+
+    private byte[] data = new byte[1024];//这个是一个数据容器
+    private byte[] nicknamedata = new byte[1024];//这个是一个数据容器
+    private Socket socketclient;
+    private Thread tt;
+
+    public ForClient(Socket s)
+    {
+        socketclient = s;
+        //启动一个线程，处理客户端数据接收
+
+        tt = new Thread(ReceiveMessage);
+        tt.Start();
+    }
+
+    void ReceiveMessage()
+    {
+        //一直接收客户端的数据
+        while (true)
+        {
+            //在接收数据之前  判断一下socket连接是否断开
+            if (socketclient.Poll(10, SelectMode.SelectRead))
+            {
+                socketclient.Close();
+                break;//跳出循环 终止线程的执行
+            }
+            else
+            {
+
+                try
+                {
+                    int length = socketclient.Receive(data);
+                    string message = Encoding.UTF8.GetString(data, 0, length);
+
+                    int nickname_length = socketclient.Receive(nicknamedata);
+                    string nickname = Encoding.UTF8.GetString(nicknamedata, 0, nickname_length);
+
+
+                    //接收到数据的时候 要把这个数据 分发到客户端
+                    //广播这个消息
+                    Program.BroadcastMessage(nickname + " > " + message);
+                    Console.WriteLine("收到了客户端：" + socketclient.RemoteEndPoint + "，昵称为："+ nickname + "，消息:" + message);
+                }
+
+                catch
+                {
+                    Console.WriteLine("出错了");
+                }
+            }
+        }
+    }
+
+    public void SendMessage(string message)
+    {
+
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        socketclient.Send(data);
+    }
+    public bool Connected
+    {
+        get { return socketclient.Connected; }
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Net;
+using System;
+
+class Program
+{
+    static List<ForClient> clientlist = new List<ForClient>();
+    static void Main(string[] args)
+    {
+
+        Socket TcpServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        TcpServer.Bind(new IPEndPoint(IPAddress.Parse("0.0.0.0"), 2000));
+        TcpServer.Listen(100);
+        Console.WriteLine("服务器开启");
+
+        while (true)
+        {
+            Socket clientSocket = TcpServer.Accept();
+            Console.WriteLine("新接入一个客户端");
+            ForClient cc = new ForClient(clientSocket);//每个客户端通信逻辑放到client
+            clientlist.Add(cc);
+        }
+
+    }
+
+    public static void BroadcastMessage(string message)
+    {
+        var notConnectedList = new List<ForClient>();
+
+        foreach (var clients in clientlist)
+        {
+            if (clients.Connected)
+                clients.SendMessage(message);
+            else
+            {
+                notConnectedList.Add(clients);
+            }
+        }
+        foreach (var temp in notConnectedList)
+        {
+            clientlist.Remove(temp);
+        }
+    }
+}
