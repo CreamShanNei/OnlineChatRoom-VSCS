1b04962c9f7d412890022bee62eb340a07515c25
ZeroShanNei <3122548531@qq.com>
2022-04-15 14:14:19 +0800

    4月15日 加入端口IP自定义

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -8,11 +8,15 @@ class Program
     static List<ForClient> clientlist = new List<ForClient>();
     static void Main(string[] args)
     {
-
+        Console.WriteLine("输入服务端监听IP");
+        string ipadd = Console.ReadLine();
+        Console.WriteLine("输入服务端监听端口");
+        int port = Console.Read();
         Socket TcpServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        TcpServer.Bind(new IPEndPoint(IPAddress.Parse("0.0.0.0"), 2000));
+        TcpServer.Bind(new IPEndPoint(IPAddress.Parse(ipadd),port));
         TcpServer.Listen(100);
         Console.WriteLine("服务器开启");
+        Console.WriteLine("监听端口：" + ipadd + ":" + port.ToString());
 
         while (true)
         {
